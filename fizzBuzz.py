c41bb8eab81365b6cbf3bd1c4108d1f8030eeecc
katouyo <yothiac13@gmail.com>
2018-11-17 14:53:25 +0900

    reformat test

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,10 +1,16 @@
 inputNumber = int(input("正の整数を入れてね: "))
 
-if inputNumber % 3 == 0:
+if inputNumber % 15 == 0:
+    output = "FizzBuzz"
+
+elif inputNumber % 3 == 0:
     output = "Fizz"
 
 elif inputNumber % 5 == 0:
     output = "Buzz"
+
+elif inputNumber % 7 == 0:
+    output = "aaaa"
 
 else:
     output = str(inputNumber)
