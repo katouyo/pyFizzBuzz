448f477a4b9f0bd52bb46b70e23b3a3a71fad526
katouyo <yothiac13@gmail.com>
2018-11-17 17:18:28 +0900

    for文でまわす

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -13,19 +13,6 @@ def convertFizzBuzz(number):
     return str(number)
 
 
-print(convertFizzBuzz(1))
-print(convertFizzBuzz(2))
-print(convertFizzBuzz(3))
-print(convertFizzBuzz(4))
-print(convertFizzBuzz(5))
-print(convertFizzBuzz(6))
-print(convertFizzBuzz(7))
-print(convertFizzBuzz(8))
-print(convertFizzBuzz(9))
-print(convertFizzBuzz(10))
-print(convertFizzBuzz(11))
-print(convertFizzBuzz(12))
-print(convertFizzBuzz(13))
-print(convertFizzBuzz(14))
-print(convertFizzBuzz(15))
-print(convertFizzBuzz(16))
+for number in range(1, 101):
+    result = convertFizzBuzz(number)
+    print(result)
