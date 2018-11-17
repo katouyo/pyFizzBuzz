47b94263ee4644b0d08f87ed11157810fd2df77d
katouyo <yothiac13@gmail.com>
2018-11-17 16:54:03 +0900

    convertFizzBuzzを追加

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,13 +1,32 @@
-number = int(input("正の整数を入れてね: "))
+# number = int(input("正の整数を入れてね: "))
 
-if number % 15 == 0:
-    print("FizzBuzz")
+def convertFizzBuzz(number):
+    if number % 15 == 0:
+        return "FizzBuzz"
 
-elif number % 3 == 0:
-    print("Fizz")
+    elif number % 3 == 0:
+        return "Fizz"
 
-elif number % 5 == 0:
-    print("Buzz")
+    elif number % 5 == 0:
+        return "Buzz"
 
-else:
-    print(str(number))
+    else:
+        return str(number)
+
+
+print(convertFizzBuzz(1))
+print(convertFizzBuzz(2))
+print(convertFizzBuzz(3))
+print(convertFizzBuzz(4))
+print(convertFizzBuzz(5))
+print(convertFizzBuzz(6))
+print(convertFizzBuzz(7))
+print(convertFizzBuzz(8))
+print(convertFizzBuzz(9))
+print(convertFizzBuzz(10))
+print(convertFizzBuzz(11))
+print(convertFizzBuzz(12))
+print(convertFizzBuzz(13))
+print(convertFizzBuzz(14))
+print(convertFizzBuzz(15))
+print(convertFizzBuzz(16))
